6e9b2b04d9199ab7f0ad6fcd196cdc9833305df7
Kenil Patel <kenilp1911@gmail.com>
2025-03-14 20:31:08 +0530

    change logic and and module selected option

--- script.js
+++ script.js
@@ -7,56 +7,24 @@ function start() {
     let start = document.getElementById("starter");
     start.style.cssText = 'top : -135px; transform : scale(0.1) ;visibility:hidden';
     event();
+}
 
+function Display_selection (value,res){
+    const choices = ['stone', 'scissor', 'paper'];
+    document.getElementById("four").innerHTML = `<img src='images/${choices[value]}.png'>`;
+    document.getElementById("result").innerHTML = `<img src='images/${choices[res]}.png'>`;
 }
 
 function user() {
     let value = this.value;
-    console.log(value);
     let res = Math.floor(Math.random() * 3);
-    console.log(res);
     let popup = document.getElementById("popup");
-
-
     iteration += 1;
-
-
     if (iteration <= 3) {
-
-
-        // your own input shown 
-        if (value == 0) {
-
-            document.getElementById("four").innerHTML = "<img src='images/stone.png'>"
-
-        }
-        else if (value == 1) {
-            document.getElementById("four").innerHTML = "<img src='images/scissor.png' >"
-
-
-        } else if (value == 2) {
-            document.getElementById("four").innerHTML = "<img src='images/paper.png'>"
-
-        }
-
-
-
-
-
-        if (res == 0) {
-
-            document.getElementById("result").innerHTML = "<img src='images/stone.png'>"
-
-        }
-        else if (res == 1) {
-            document.getElementById("result").innerHTML = "<img src='images/scissor.png' >"
-
-
-        } else if (res == 2) {
-            document.getElementById("result").innerHTML = "<img src='images/paper.png'>"
-        }
-
-        // point counting        
+        console.log(value)
+        Display_selection(value, res)
+       
+                
 
         if (value == res) {
             human += 1;
@@ -73,31 +41,30 @@ function user() {
 
         // showing star for human
 
-        if (human == 1) {
-            document.getElementById("h1").innerHTML = "<img src='images/star.webp'>";
-
+        switch(human){
+            case 1:
+                document.getElementById("h1").innerHTML = "<img src='images/star.webp'>";
+                break;
+            case 2:
+                document.getElementById("h2").innerHTML = "<img src='images/star.webp'>";
+                break;
+            case 3:
+                document.getElementById("h3").innerHTML = "<img src='images/star.webp'>";
+                break;
         }
-        else if (human == 2) {
-            document.getElementById("h2").innerHTML = "<img src='images/star.webp'>";
-        }
-        else if (human == 3) {
-            document.getElementById("h3").innerHTML = "<img src='images/star.webp'>";
-
-        }
-
         // showing star for computer
 
-        if (computer == 1) {
-            document.getElementById("c1").innerHTML = "<img src='images/star.webp'>";
+        switch(computer){
+            case 1:
+                document.getElementById("c1").innerHTML = "<img src='images/star.webp'>";
+                break;
+            case 2:
+                document.getElementById("c2").innerHTML = "<img src='images/star.webp'>";
+                break;
+            case 3:
+                document.getElementById("c3").innerHTML = "<img src='images/star.webp'>";
+                break;
         }
-        else if (computer == 2) {
-            document.getElementById("c2").innerHTML = "<img src='images/star.webp'>";
-        }
-        else if (computer == 3) {
-            document.getElementById("c3").innerHTML = "<img src='images/star.webp'>";
-
-        }
-
 
         // result
 
@@ -116,23 +83,13 @@ function user() {
 
             popup.style.cssText = 'top : 250px; transform : scale(1) ;visibility:visible';
 
-
-
         }
-    }
-    else {
-        console.log("round is over ")
     }
 }
 
 let button = document.getElementsByClassName("button");
-
-
 function event(){
 for (var i = 0; i < 3; i++) {
-
-
     button[i].addEventListener("click", user);
-
 }
 }
